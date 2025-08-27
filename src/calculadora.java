de79dd60f70ae04bd057e03c93404088f46c717f
lipestefen <felilegabriel313@gmail.com>
2025-08-27 13:57:29 -0300

    multiplicação

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,5 +1,6 @@
 public class calculadora {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static int multiplicacao(int a, int b) {
+        return a * b;
     }
 }
+
