b9592adf0bf2c5e48f52207ce0e0446ca7fe58a8
lipestefen <felilegabriel313@gmail.com>
2025-08-27 13:58:22 -0300

    divisao

--- src/calculadora.java
+++ src/calculadora.java
@@ -2,5 +2,9 @@ public class calculadora {
     public static int multiplicacao(int a, int b) {
         return a * b;
     }
+    public static double divisao(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Divisão por zero não permitida!");
+        return (double) a / b;
+    }
 }
 
