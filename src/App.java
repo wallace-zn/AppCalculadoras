ea54b48677850ac212e2e947fab56ef9f63e9a23
wallace-zn <wallace_m_rosa@estudante.sesisenai.org.br>
2025-08-27 14:23:33 -0300

    testes
    final e testamentos

--- src/App.java
+++ src/App.java
@@ -1,3 +1,8 @@
 public class App {
-    
+    public static void main(String[] args) {
+        System.out.println("Soma: " + Calculadora.soma(10, 5));
+        System.out.println("Subtração: " + Calculadora.subtracao(10, 5));
+        System.out.println("Multiplicação: " + Calculadora.multiplicacao(10, 5));
+        System.out.println("Divisão: " + Calculadora.divisao(10, 5));
+    }
 }
--- src/calculadora.java
+++ src/calculadora.java
@@ -1,19 +1,18 @@
-public class calculadora {
-<<<<<<< HEAD
+public class Calculadora {
     public static int soma(int a, int b) {
         return a + b;
-=======
+    }
     public static int multiplicacao(int a, int b) {
         return a * b;
     }
     public static double divisao(int a, int b) {
         if (b == 0) throw new IllegalArgumentException("Divisão por zero não permitida!");
         return (double) a / b;
->>>>>>> feature-multiplicacao-divisao
     }
     public static int subtracao(int a, int b) {
         return a - b;
     }
    
 }
+
 
