5c383819966518d9a22172bc4c1e484b5a095cde
wallace-zn <wallace_m_rosa@estudante.sesisenai.org.br>
2025-08-27 13:56:15 -0300

    subtração

--- src/calculadora.java
+++ src/calculadora.java
@@ -2,6 +2,8 @@ public class calculadora {
     public static int soma(int a, int b) {
         return a + b;
     }
-
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
    
 }
